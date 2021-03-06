dfdcd7a4f66fb64bbbd7e7d223bcce885be3d917
Fede Camara Halac <camarafede@gmail.com>
2021-03-06 18:24:51 -0500

    get device x y using p5

--- public/draw.js
+++ public/draw.js
@@ -6,27 +6,27 @@ function setup() {
 	frameRate(30);
 }
 function draw() {
-	col = map(mouseY, 0, height, 255, 0);
-	num = map(mouseX, 0, width, 2, 14);
-	hexagon(col,num);
+	col = map(mouseY, 0, height, 1, 0);
+	num = map(mouseX, 0, width, 1, 0);
+	console.log(col,num);
 }
-function hexagon(col,num,i) {
-	i = frameCount * 0.1;
-	translate(width/2,height/2);
-	rotate(i);
-	angle = TWO_PI / num;
-	if (radius < 0) {
-		radius = 0;
-		noLoop();
-	} else { 
-	radius = height/2 - i;
-	}
-	fill(col);
-	beginShape();
-	for (a = 0; a < TWO_PI; a += angle) {
-		sx = cos(a) * radius + (Math.random()*2);
-		sy = sin(a) * radius + (Math.random()*2);
-		vertex(sx, sy);
-	}
-	endShape(CLOSE);
-}
+// function hexagon(col,num,i) {
+// 	i = frameCount * 0.1;
+// 	translate(width/2,height/2);
+// 	rotate(i);
+// 	angle = TWO_PI / num;
+// 	if (radius < 0) {
+// 		radius = 0;
+// 		noLoop();
+// 	} else { 
+// 	radius = height/2 - i;
+// 	}
+// 	fill(col);
+// 	beginShape();
+// 	for (a = 0; a < TWO_PI; a += angle) {
+// 		sx = cos(a) * radius + (Math.random()*2);
+// 		sy = sin(a) * radius + (Math.random()*2);
+// 		vertex(sx, sy);
+// 	}
+// 	endShape(CLOSE);
+// }
--- public/webclient.js
+++ public/webclient.js
@@ -187,4 +187,42 @@ function startSocket() {
   socket = io({transports: ['websocket']});
 }
 
+var canvas
+var i, a;
+var sx,sy,angle,radius,click;
 
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  frameRate(30);
+}
+function draw() {
+  col = map(mouseY, 0, height, 255, 0);
+  num = map(mouseX, 0, width, 1, 14);
+  console.log(col,num);
+  hexagon(col,num,i);
+  if (status = "deviceDoesNotReportMotion") {
+    x = mouseX;
+    y = mouseY;
+  }
+
+}
+function hexagon(col,num,i) {
+ i = frameCount * 0.1;
+ translate(width/2,height/2);
+ rotate(i);
+ angle = TWO_PI / num;
+ if (radius < 0) {
+   radius = 0;
+   noLoop();
+ } else { 
+ radius = height/2 - i;
+ }
+ fill(col);
+ beginShape();
+ for (a = 0; a < TWO_PI; a += angle) {
+   sx = cos(a) * radius + (Math.random()*2);
+   sy = sin(a) * radius + (Math.random()*2);
+   vertex(sx, sy);
+ }
+ endShape(CLOSE);
